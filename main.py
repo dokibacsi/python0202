4c1664bbeb87c61e26af6e2cb543b013c6695898
dokibacsi <kvalcsi02@gmail.com>
2023-02-02 16:12:54 +0100

    tovabbi tesztesetek

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-def eredmeny(jatekosLapok: [int], gepLapok: [int]):
-    pass
-
 def osszeg(kartyak: [int]):
     pontok: int = 0
     for i in range(len(kartyak)):
         pontok += kartyak[i]
     return pontok
+
+def eredmeny(jatekosLapok: [int], gepLapok: [int]):
+    pass
+
+#jatekos vesztett
 
 def jatekos_vesztett_tobbMintHuszonegy():
     jatekos = [10, 9, 6]
@@ -38,7 +40,7 @@ def jatekos_vesztett_tobbLapbolHuszonegy():
     else:
         print("[Több lapból 21 tesztje]: megbukott")
 
-def jatekos_vesztett_kisebbEredmenyMintAGepnek():
+def jatekos_vesztett_kisebbEredmeny():
     jatekos = [10, 8]
     gep = [10, 10]
     vart_eredmeny = "jatekos vesztett"
@@ -48,10 +50,62 @@ def jatekos_vesztett_kisebbEredmenyMintAGepnek():
     else:
         print("[Kisebb eredmény tesztje]: megbukott")
 
+#gep vesztett
+
+def gep_vesztett_tobbMintHuszonegy():
+    jatekos = [10, 9, 2]
+    gep = [10, 8, 6]
+    vart_eredmeny = "gep vesztett"
+    kapott_eredmeny = eredmeny(jatekos, gep)
+    if kapott_eredmeny == vart_eredmeny:
+        print("[Kisebb eredmény tesztje]: teszt sikeres")
+    else:
+        print("[Kisebb eredmény tesztje]: megbukott")
+
+def gep_vesztett_kevesebbMintHuszonegy():
+    jatekos = [10, 9, 2]
+    gep = [10, 8, 6]
+    vart_eredmeny = "gep vesztett"
+    kapott_eredmeny = eredmeny(jatekos, gep)
+    if kapott_eredmeny == vart_eredmeny:
+        print("[Kisebb eredmény tesztje]: teszt sikeres")
+    else:
+        print("[Kisebb eredmény tesztje]: megbukott")
+
+def gep_vesztett_tobbLapbolHuszonegy():
+    jatekos = [10, 3, 6, 2]
+    gep = [4, 3, 3, 4, 2, 3, 2]
+    vart_eredmeny = "gep vesztett"
+    kapott_eredmeny = eredmeny(jatekos, gep)
+    if kapott_eredmeny == vart_eredmeny:
+        print("[Több lapból 21 tesztje]: teszt sikeres")
+    else:
+        print("[Több lapból 21 tesztje]: megbukott")
+
+def gep_vesztett_kisebbEredmeny():
+    jatekos = [10, 10]
+    gep = [10, 8]
+    vart_eredmeny = "gep vesztett"
+    kapott_eredmeny = eredmeny(jatekos, gep)
+    if kapott_eredmeny == vart_eredmeny:
+        print("[Kisebb eredmény tesztje]: teszt sikeres")
+    else:
+        print("[Kisebb eredmény tesztje]: megbukott")
+
+
 def teszt_esetek():
+
+    #Játékos vesztett esetek:
+
     jatekos_vesztett_tobbMintHuszonegy()
     jatekos_vesztett_kevesebbMintHuszonegy()
     jatekos_vesztett_tobbLapbolHuszonegy()
-    jatekos_vesztett_kisebbEredmenyMintAGepnek()
+    jatekos_vesztett_kisebbEredmeny()
+
+    #Gép vesztett esetek:
+    gep_vesztett_tobbMintHuszonegy()
+    gep_vesztett_kevesebbMintHuszonegy()
+    gep_vesztett_tobbLapbolHuszonegy()
+    gep_vesztett_kisebbEredmeny()
 
 teszt_esetek()
