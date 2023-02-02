286d1c3a31149865d0a00d1777b8501ea42d2e64
dokibacsi <kvalcsi02@gmail.com>
2023-02-02 15:22:50 +0100

    jatekos_vesztett metódus

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 def eredmeny():
     pass
+
+def osszeg(kartyak: [int]):
+    pontok: int = 0
+    for i in range(len(kartyak)):
+        pontok += kartyak[i]
+    return pontok
+
+def teszt_esetek():
+    jatekos_vesztett()
+teszt_esetek()
+
+def jatekos_vesztett():
+    jatekos = [10, 9, 3]
+    gep = [10, 9]
+    vart_eredmeny = "jatekos vesztett"
+    kapott_eredmeny = eredmeny(jatekos, gep)
+    if kapott_eredmeny == vart_eredmeny:
+        print("teszt sikeres")
+    else:
+        print("megbukott")
