55d18c6227d6a53331e2a3d32c94890f2d1c7367
dokibacsi <kvalcsi02@gmail.com>
2023-02-02 15:46:37 +0100

    jatekos tovabbi tesztesetek

--- main.py
+++ main.py
@@ -7,13 +7,9 @@ def osszeg(kartyak: [int]):
         pontok += kartyak[i]
     return pontok
 
-def teszt_esetek():
-    jatekos_vesztett()
-teszt_esetek()
-
-def jatekos_vesztett():
-    jatekos = [10, 9, 3]
-    gep = [10, 9]
+def jatekos_vesztett_tobbMintHuszonegy():
+    jatekos = [10, 9, 6]
+    gep = [10, 10]
     vart_eredmeny = "jatekos vesztett"
     kapott_eredmeny = eredmeny(jatekos, gep)
     if kapott_eredmeny == vart_eredmeny:
@@ -21,3 +17,41 @@ def jatekos_vesztett():
     else:
         print("megbukott")
     pass
+
+def jatekos_vesztett_kevesebbMintHuszonegy():
+    jatekos = [10, 8]
+    gep = [10, 9, 2]
+    vart_eredmeny = "jatekos vesztett"
+    kapott_eredmeny = eredmeny(jatekos, gep)
+    if kapott_eredmeny == vart_eredmeny:
+        print("teszt sikeres")
+    else:
+        print("megbukott")
+
+def jatekos_vesztett_tobbLapbolHuszonegy():
+    jatekos = [10, 5, 2, 4]
+    gep = [10, 9, 2]
+    vart_eredmeny = "jatekos vesztett"
+    kapott_eredmeny = eredmeny(jatekos, gep)
+    if kapott_eredmeny == vart_eredmeny:
+        print("teszt sikeres")
+    else:
+        print("megbukott")
+
+def jatekos_vesztett_kisebbEredmenyMintAGepnek():
+    jatekos = [10, 8]
+    gep = [10, 10]
+    vart_eredmeny = "jatekos vesztett"
+    kapott_eredmeny = eredmeny(jatekos, gep)
+    if kapott_eredmeny == vart_eredmeny:
+        print("teszt sikeres")
+    else:
+        print("megbukott")
+
+def teszt_esetek():
+    jatekos_vesztett_tobbMintHuszonegy()
+    jatekos_vesztett_kevesebbMintHuszonegy()
+    jatekos_vesztett_tobbLapbolHuszonegy()
+    jatekos_vesztett_kisebbEredmenyMintAGepnek()
+
+teszt_esetek()
